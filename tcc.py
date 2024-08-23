b79fca5be3e2d4656d3446af3f7bfbe35dfea87e
Leo20031999 <leofridman@outlook.com>
2024-08-23 17:12:28 -0300

    Adição de altura de vertice

--- tcc.py
+++ tcc.py
@@ -8,7 +8,17 @@ class Graph:
         self.adj_list[u].append(v)
         
     def print_graph(self):
-        for vertex, adjacent in self.adj_list.items():
+        allVertices = set(self.adj_list.keys())
+        for neighbours in self.adj_list.values():
+            allVertices.update(neighbours)
+        adjacencyList = {vertex: set() for vertex in allVertices}
+
+        for vertex, neighbours in self.adj_list.items():
+            for neighbour in neighbours:
+                adjacencyList[vertex].add(neighbour)
+                adjacencyList[neighbour].add(vertex)
+        for vertex in sorted(adjacencyList):
+            adjacent = sorted(adjacencyList[vertex])
             print(f'{vertex}: {"->".join(map(str,adjacent))}')
 
     def is_leaf(self, u):
@@ -32,21 +42,40 @@ class Graph:
                     del lista[leaf]
             leafs = new_leafs
         return list(lista.keys())
+    
+    def calculateHeight(self, node, parent=None):
+        height = 0
+        for neighbor in self.adj_list.get(node, []):
+            if neighbor != parent:
+                height = max(height, self.calculateHeight(neighbor, node) + 1)
+        return height
+
+    def heights(self):
+        heights = {}
+        allVertices = set(self.adj_list.keys())
+        for vertex in self.adj_list.values():
+            allVertices.update(vertex)
+        for vertex in allVertices:
+            heights[vertex] = self.calculateHeight(vertex,None)
+        return heights
 
 def hausdorffDistanceBetweenTrees(grafo1, grafo2):
-    hd = 99
-    O = 0
-    list_v = grafo1.center()
-    v = list_v[0]
-    
+    return 0   
 
-grafo = Graph()
+graph = Graph()
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
+graph.add_edge(1, 3)
+graph.add_edge(1, 4)
+graph.add_edge(2, 5)
+graph.add_edge(2, 6)
+graph.add_edge(5, 7)
+graph.add_edge(6, 8)
 
-grafo.add_edge('A', 'B')
-grafo.add_edge('B', 'C')
-grafo.add_edge('B', 'D')
-grafo.add_edge('D', 'E')
-grafo.add_edge('D', 'F')
+print("Representação do Grafo:")
+graph.print_graph()
 
-grafo.print_graph()
-print(grafo.center())
+heights = graph.heights()
+print("\nAlturas dos vértices:")
+for vertex, height in heights.items():
+    print(f"Vértice {vertex}: Altura {height}")
